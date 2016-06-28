aa632505adbd6f2300bedbdcbd3f65bfcd2133cb
joe-broder15 <joey.broder@gmail.com>
2016-06-28 09:24:05 -0700

    Second commit
    this is a test with git

--- Tip Pro/ViewController.swift
+++ Tip Pro/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //UI elements
+    @IBOutlet weak var billAmountFeild: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountFeild: UITextField!
+    @IBOutlet weak var totalAmountFeild: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,52 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return.LightContent
+    }
 
+    //when the calculate button is pressed
+    @IBAction func calculateTip(sender: AnyObject) {
+    
+        //Tip calculation goes here
+        
+        //If text feilds are not numbers, make them blank
+        guard let billAmount = Double(billAmountFeild.text!) else{
+            billAmountFeild.text = ""
+            tipAmountFeild.text = ""
+            totalAmountFeild.text = ""
+            return
+        }
+        
+        //Sets the tip percentage based on the segment chosen
+        var tipPercentage = 0.0
+        
+        switch tipSelector.selectedSegmentIndex{
+        case 0:
+            tipPercentage = 0.15
+        case 1:
+            tipPercentage = 0.18
+        case 2:
+            tipPercentage = 0.2
+        default:
+            break
+        }
+        
+        let roundedBillAmount = round(billAmount*100)/100
+        let tipAmount = roundedBillAmount * tipPercentage
+        let roundedTipAmount = round(tipAmount*100)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if (!billAmountFeild.editing){
+                billAmountFeild.text = String(format: "%.2f", roundedBillAmount )
+        }
+        
+        tipAmountFeild.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountFeild.text = String(format: "%.2f", totalAmount)
+        
+    }
+    
 
 }
 
